03c7234d45ca2c2269c2a67e68d42063d6aa24e0
PILLAGOVLA MAHESH <137051507+PillagovulaMahesh@users.noreply.github.com>
2025-10-01 20:20:35 +0530

    Update next.config.js

---
+++ Yebelo_Trading_Assignment/frontend/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: false, // Keeping pages/ directory enabled
+  },
+};
+
+module.exports = nextConfig;
